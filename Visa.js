b9ecce728dc9474eb6c78355c6c28fb3bd370187
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-03-03 17:20:39 -0800

    Create Visa.js

---
+++ Visa.js
@@ -0,0 +1,8 @@
+<script>
+  
+//Get the date of entry from the user
+var d = new Date();
+//Get the number of days from the user
+d.setDate(d.getDate() + 50);
+document.getElementById("demo").innerHTML = d;
+</script>
