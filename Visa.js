11750bf2f93bcb5ccdbdcdbcfae5aab37a3901bf
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-05-11 22:31:00 -0500

    Update Visa.js

--- Visa.js
+++ Visa.js
@@ -2,8 +2,10 @@ function Checker(){
   
 //Get the date of entry from the user
 var d = new Date();
+
 //Get the number of days from the user
 d.setDate(d.getDate() + 50);
 document.getElementById("demo").innerHTML = d;
+  
 
 }
